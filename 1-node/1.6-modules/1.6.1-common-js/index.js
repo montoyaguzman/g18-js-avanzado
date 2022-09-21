cb301c4c1ebd6451528fa3b5e9fb296169884e1b
montoyaguzman <montoyaguzman7@gmail.com>
2022-09-20 19:42:51 -0500

    feat: ejemplo de exports/require con common-js

--- 1-node/1.6-modules/1.6.1-common-js/index.js
+++ 1-node/1.6-modules/1.6.1-common-js/index.js
@@ -1,13 +1,13 @@
-
+const operations = require('./functions');
 
 function main() {
     let numero1 = 10;
     let numero2 = 40;
 
-    const resultSum = sum(numero1, numero2);
-    const resultSubs = substract(numero1, numero2);
-    const resultMult = multiply(numero1, numero2);
-    const resultDiv = divideNumbers(numero1, numero2);
+    const resultSum = operations.sum(numero1, numero2);
+    const resultSubs = operations.substract(numero1, numero2);
+    const resultMult = operations.multiply(numero1, numero2);
+    const resultDiv = operations.divideNumbers(numero1, numero2);
 
     console.log('suma: ', resultSum);
     console.log('resta: ', resultSubs);
